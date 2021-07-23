bed19c71b6ad92af0437d5dbe54c2be7d8fddfda
Kevin <kevisrich144@gmail.com>
2021-07-23 15:29:27 +0100

    Jarvis Encrypt and Decrypt file

--- main.py
+++ main.py
@@ -83,6 +83,7 @@ print("10. Square rooting")
 print("11. Median")
 print("12. Factors")
 print("13. Mean")
+print("14. File Encryption")
 yourchoice = int(sys.stdin.readline())
 
 if yourchoice == 1:
@@ -236,6 +237,68 @@ if yourchoice == 13:
     statistics.mean(Numbers)
     print(statistics.mean(Numbers))
 
+if yourchoice == 14:
+    print("Welcome in Ceasar encryptor!")
+    print("Choose option")
+    print("1. Encrypt message")
+    print("2. Decrypt message")
+    choice = int(input())
+
+
+    def ceasarEncrypt(message, shift):
+        message = message.upper()
+        alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+        result = ""
+
+        for letter in message:
+            if letter in alphabet:
+                letter_index = (alphabet.find(letter) + shift) % len(alphabet)
+                result = result + alphabet[letter_index]
+            else:
+                result = result + letter
+        return result
+
+
+    def ceasarDecrypt(message, shift):
+        message = message.upper()
+        alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+        result = ""
+
+        for letter in message:
+            if letter in alphabet:
+                letter_index = (alphabet.find(letter) - shift) % len(alphabet)
+                result = result + alphabet[letter_index]
+            else:
+                result = result + letter
+        return result
+
+
+    if choice == 1:
+        print("Give me txt file name:")
+        fileName = input()
+        print("Which shift?")
+        shift = int(input())
+        file = open(fileName)
+        content = file.read()
+        result = ceasarEncrypt(content, shift)
+        print(result)
+        file.close()
+        outputFile = open('output.txt', 'w')
+        outputFile.write(result)
+        outputFile.close()
+
+    elif choice == 2:
+        print("Give me txt file name:")
+        fileName = input()
+        print("Which shift?")
+        shift = int(input())
+        file = open(fileName)
+        content = file.read()
+        result = ceasarDecrypt(content, shift)
+        print(result)
+
+    else:
+        print("Invalid number")
 
 
 
