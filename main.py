7b449b4399f71ba5d3f3ee8ac39a3ec9b7297b07
Kevin <kevisrich144@gmail.com>
2021-10-02 12:44:33 +0100

    Buttons Now working with all commands in place.

--- main.py
+++ main.py
@@ -13,140 +13,81 @@ start_screen.pack()
 start_screen.create_text(200,20, text='Welcome',font=('Times', 20))
 start_screen.create_text(200,50, text='Hello, Im Jarvis, your personal assistant :-)',font=('Times', 15))
 start_screen.create_text(200,80, text='What can I do for you today?',font=('Times', 15))
-def click():
-    print("Welcome")
-button = Button(start_tk, text="Show my favorites fruites", command=click)
-button1 = Button(start_tk, text="Show my favorite actors", command=click)
-button2 = Button(start_tk, text="Generate weapon", command=click)
-button3 = Button(start_tk, text="Give me 2 number for addition", command=click)
-button4 = Button(start_tk, text="Give me 2 number for subtraction", command=click)
-button5 = Button(start_tk, text="Give me 2 number for multiplication", command=click)
-button6 = Button(start_tk, text="Give me 2 number for division", command=click)
-button7 = Button(start_tk, text="Algebra", command=click)
-button8 = Button(start_tk, text="Fractions", command=click)
-button9 = Button(start_tk, text="Square rooting", command=click)
-button10 = Button(start_tk, text="Median", command=click)
-button11 = Button(start_tk, text="Factors", command=click)
-button12 = Button(start_tk, text="Mean", command=click)
-button13 = Button(start_tk, text="File Encryption", command=click)
-button14 = Button(start_tk, text="Keyword", command=click)
-button15 = Button(start_tk, text="Random shapes and colors", command=click)
-button16 = Button(start_tk, text="Jarvis singing happy birthday", command=click)
-button.pack()
-button1.pack()
-button2.pack()
-button3.pack()
-button4.pack()
-button5.pack()
-button6.pack()
-button7.pack()
-button8.pack()
-button9.pack()
-button10.pack()
-button11.pack()
-button12.pack()
-button13.pack()
-button14.pack()
-button15.pack()
-button16.pack()
-start_tk.mainloop()
-Fruits = ["Apple","Watermelon","Banana","Blueberrie"]
-Actors = {'harry potter' : 'Harry Potter and the Goblet of Fire (2005)',
-        'Christopher Daniel Barnes' : 'Spiderman',
-        'Jason Farries' : 'InspectorCalls'}
 Numbers = []
+
 def printElement(list):
     for i in list:
         print(i)
-
-# printElement(Fruits)
-# printElement(Actors)
 
 class AI:
     pass
 robot = AI()
 
-def generatearrow():
-    drawer = turtle.Pen()
-    drawer.forward(90)
-    turtle.mainloop()
+def fruits():
+    Fruits = ["Apple", "Watermelon", "Banana", "Blueberrie"]
+    printElement(Fruits)
 
-def trident():
-  ewa = turtle.Pen()
-  ewa.forward(50)
-  ewa.right(90)
-  ewa.left(180)
-  ewa.forward(40)
+def actors():
+    Actors = {'harry potter': 'Harry Potter and the Goblet of Fire (2005)',
+              'Christopher Daniel Barnes': 'Spiderman',
+              'Jason Farries': 'InspectorCalls'}
+    printElement(Actors)
 
-  kasia = turtle.Pen()
-  kasia.left(90)
-  kasia.forward(130)
-  kasia.left(180)
-  kasia.forward(400)
-  kasia.left(180)
-  kasia.forward(400)
-
-  jakub = turtle.Pen()
-  jakub.forward(80)
-  jakub.left(90)
-  jakub.forward(90)
-
-  maja = turtle.Pen()
-  maja.left(180)
-  maja.forward(90)
-  maja.right(90)
-  maja.forward(90)
-
-  tata = turtle.Pen()
-  tata.left(180)
-  tata.forward(40)
-  tata.right(90)
-  tata.forward(40)
-  turtle.mainloop()
-
-def generatesword():
-    drawer = turtle.Pen()
-    drawer.forward(90)
-    drawer.left(180)
-    drawer.forward(20)
-    drawer.right(90)
-    drawer.forward(20)
-    drawer.left(180)
-    drawer.forward(40)
-    turtle.mainloop()
-
-#
-# print("1. Show my favorites fruites.")
-# print("2. Show my favorite actors.")
-# print("3. Generate weapon.")
-# print("4. Give me 2 number for addition")
-# print("5. Give me 2 number for subtraction")
-# print("6. Give me 2 number for multiplication")
-# print("7. Give me 2 number for division")
-# print("8. Algebra")
-# print("9. Fractions")
-# print("10. Square rooting")
-# print("11. Median")
-# print("12. Factors")
-# print("13. Mean")
-# print("14. File Encryption")
-# print("15. keyword")
-# print("16. Random shapes and colors")
-# print("17. Jarvis singing happy birthday")
-yourchoice = int(sys.stdin.readline())
-
-if yourchoice == 1:
-  printElement(Fruits)
-
-elif yourchoice == 2:
-  printElement(Actors)
-
-elif yourchoice == 3:
+def weapons():
   print("Which weapon should I generate?\n")
   print("1. Arrow?")
   print("2. Sword?")
   print("3. Trident")
   yourchoice = int(sys.stdin.readline())
+
+  def generatearrow():
+      drawer = turtle.Pen()
+      drawer.forward(90)
+      turtle.mainloop()
+
+  def trident():
+      ewa = turtle.Pen()
+      ewa.forward(50)
+      ewa.right(90)
+      ewa.left(180)
+      ewa.forward(40)
+
+      kasia = turtle.Pen()
+      kasia.left(90)
+      kasia.forward(130)
+      kasia.left(180)
+      kasia.forward(400)
+      kasia.left(180)
+      kasia.forward(400)
+
+      jakub = turtle.Pen()
+      jakub.forward(80)
+      jakub.left(90)
+      jakub.forward(90)
+
+      maja = turtle.Pen()
+      maja.left(180)
+      maja.forward(90)
+      maja.right(90)
+      maja.forward(90)
+
+      tata = turtle.Pen()
+      tata.left(180)
+      tata.forward(40)
+      tata.right(90)
+      tata.forward(40)
+      turtle.mainloop()
+
+  def generatesword():
+      drawer = turtle.Pen()
+      drawer.forward(90)
+      drawer.left(180)
+      drawer.forward(20)
+      drawer.right(90)
+      drawer.forward(20)
+      drawer.left(180)
+      drawer.forward(40)
+      turtle.mainloop()
 
   if yourchoice == 1:
     generatearrow()
@@ -157,28 +98,31 @@ elif yourchoice == 3:
   elif yourchoice == 3:
     trident()
 
-elif yourchoice == 4:
+def adding():
     print("Please give me your first number")
     number1 = int(sys.stdin.readline())
     print("Please give me yor second number")
     number2 = int(sys.stdin.readline())
     totalamount = number1 + number2
     print(totalamount)
-elif yourchoice == 6:
+
+def multiplication():
     print("Please give me your first number")
     number1 = int(sys.stdin.readline())
     print("Please give me yor second number")
     number2 = int(sys.stdin.readline())
     totalamount = number1 * number2
     print(totalamount)
-elif yourchoice == 5:
+
+def takeaway():
     print("Please give me your first number")
     number1 = int(sys.stdin.readline())
     print("Please give me yor second number")
     number2 = int(sys.stdin.readline())
     totalamount = number1 - number2
     print(totalamount)
-elif yourchoice == 7:
+
+def division():
     print("Please give me your first number")
     number1 = int(sys.stdin.readline())
     print("Please give me yor second number")
@@ -186,7 +130,7 @@ elif yourchoice == 7:
     totalamount = number1 / number2
     print(totalamount)
 
-elif yourchoice == 8:
+def algebra():
     print("Give me your x")
     number1 = float(sys.stdin.readline())
     number2 = float(sys.stdin.readline())
@@ -198,7 +142,7 @@ elif yourchoice == 8:
     number6 = float(sys.stdin.readline())
     print("Sum x %s, sum y %s, sum z %s" % ((number1+number2),(number3+number4),(number5+number6)))
 
-elif yourchoice == 9:
+def fractions():
   print("Fractions Please chose your option?\n")
   print("1. Addition?")
   print("2. Subtraction?")
@@ -250,14 +194,14 @@ elif yourchoice == 9:
               z = z * Fraction(numerator, denomenator)
           print(z)
 
-if yourchoice == 10:
+def fraction_multiplication():
     print("Please give us your number")
     number1 = int(sys.stdin.readline())
     print("Please give me your square root")
     number2 = int(sys.stdin.readline())
     print(number1 ** number2)
 
-if yourchoice == 11:
+def median():
     print("Please tell me how many numbers you want:")
     number = int(sys.stdin.readline())
     for x in range(0,number):
@@ -267,7 +211,7 @@ if yourchoice == 11:
     print(statistics.median(Numbers))
     print(sorted(Numbers))
 
-if yourchoice == 12:
+def factors():
     print("Please enter you number:")
     number1 = int(sys.stdin.readline())
     number2 = 0
@@ -276,7 +220,7 @@ if yourchoice == 12:
         number1 = number1 - 1
         number2 = number2 + 1
 
-if yourchoice == 13:
+def mean():
     print("How many numbers would you want to add and divide:")
     number = int(sys.stdin.readline())
     for x in range(0,number):
@@ -286,7 +230,7 @@ if yourchoice == 13:
     statistics.mean(Numbers)
     print(statistics.mean(Numbers))
 
-if yourchoice == 14:
+def ceasar_encryptor():
     print("Welcome in Ceasar encryptor!")
     print("Choose option")
     print("1. Encrypt message")
@@ -348,7 +292,8 @@ if yourchoice == 14:
 
     else:
         print("Invalid number")
-if yourchoice == 15:
+
+def is_keyword():
     print("Please enter a keyword: ")
     key_word = input()
     if keyword.iskeyword(key_word):
@@ -356,7 +301,7 @@ if yourchoice == 15:
     else:
         print("You can use it.")
 
-if yourchoice == 16:
+def random_shapes():
     tk = Tk()
     img = Canvas(tk, width=400, height=400)
     img.pack()
@@ -404,7 +349,7 @@ if yourchoice == 16:
     tk.mainloop()
 
 
-if yourchoice == 17:
+def birthday_song():
     tk=Tk()
     img = Canvas(tk, width=400, height=400)
     img.pack()
@@ -418,20 +363,40 @@ if yourchoice == 17:
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+button = Button(start_tk, text="Show my favorites fruites", command=fruits)
+button1 = Button(start_tk, text="Show my favorite actors", command=actors)
+button2 = Button(start_tk, text="Generate weapon", command=weapons)
+button3 = Button(start_tk, text="Give me 2 number for addition", command=adding)
+button4 = Button(start_tk, text="Give me 2 number for subtraction", command=multiplication)
+button5 = Button(start_tk, text="Give me 2 number for multiplication", command=takeaway)
+button6 = Button(start_tk, text="Give me 2 number for division", command=division)
+button7 = Button(start_tk, text="Algebra", command=algebra)
+button8 = Button(start_tk, text="Fractions", command=fractions)
+button9 = Button(start_tk, text="Square rooting", command=fraction_multiplication)
+button10 = Button(start_tk, text="Median", command=median)
+button11 = Button(start_tk, text="Factors", command=factors)
+button12 = Button(start_tk, text="Mean", command=mean)
+button13 = Button(start_tk, text="File Encryption", command=ceasar_encryptor)
+button14 = Button(start_tk, text="Keyword", command=is_keyword)
+button15 = Button(start_tk, text="Random shapes and colors", command=random_shapes)
+button16 = Button(start_tk, text="Jarvis singing happy birthday", command=birthday_song)
+button.pack()
+button1.pack()
+button2.pack()
+button3.pack()
+button4.pack()
+button5.pack()
+button6.pack()
+button7.pack()
+button8.pack()
+button9.pack()
+button10.pack()
+button11.pack()
+button12.pack()
+button13.pack()
+button14.pack()
+button15.pack()
+button16.pack()
+start_tk.mainloop()
 
 
